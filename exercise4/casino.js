7d67fa13e3f99ad7c3d9620c63325e4b53639347
veedubrunner <jon.ketchum@live.com>
2016-12-12 21:35:57 +0900

    casino finished

--- exercise4/casino.js
+++ exercise4/casino.js
@@ -14,9 +14,38 @@ function getSSN(){
 function addReceipt(){
   var recValue = document.getElementById("Receiptin");
   var recDisplay = document.getElementById("RecBox");
+  var recTotal = document.getElementById("RecTotal");
 
-
+  recTotal.value =parseInt(recTotal.value) + parseInt(recValue.value);
+  recDisplay.value = recDisplay.value + "$" + recValue.value + "\n";
 }
-function chipcounter(){
 
+function chipcounter(){
+  var Black = parseInt(document.getElementById("cBlack").value);
+  var Blue = parseInt(document.getElementById("cBlue").value);
+  var Red = parseInt(document.getElementById("cRed").value);
+  var Green = parseInt(document.getElementById("cGreen").value);
+  var Purple = parseInt(document.getElementById("cPurple").value);
+  var Gold = parseInt(document.getElementById("cGold").value);
+  var ChipTotal = document.getElementById("cTotal");
+
+  ChipTotal.value = (Black * 5) + (Blue * 20) + (Red * 50) + (Green * 100) + (Purple * 500) + (Gold * 1000);
+}
+
+function getwinnings(){
+  var SubTotal = document.getElementById("winsub");
+  var Chips = document.getElementById("cTotal");
+  var Receipts = document.getElementById("RecTotal");
+  var Tax = document.getElementById("taxes");
+  var Cashout = document.getElementById("winnings");
+  var citizen = document.getElementById("USyes");
+
+  SubTotal.value = parseInt(Chips.value) - parseInt(Receipts.value);
+
+  if(citizen.checked && SubTotal.value > 0){
+    Tax.value = 0.1 * SubTotal.value;
+  }
+  else Tax.value = 0;
+
+  Cashout.value = SubTotal.value - Tax.value;
 }
